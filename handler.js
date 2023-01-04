3dcc415b63774bdd06bf357565fdabb100714da9
Carlos Guerrero <git@carlosguerrero.com>
2023-01-04 16:51:37 +0000

    Also parse body and send in separated message

--- handler.js
+++ handler.js
@@ -5,6 +5,16 @@ const telegram = require("./telegram");
 async function webhookCenter(event) {
   console.log("🔥", JSON.stringify(event, null, 2));
   await telegram(JSON.stringify(event, null, 2));
+  if (event.body) {
+    let body = event.body;
+    try {
+      body = JSON.parse(event.body);
+    } catch (err) {
+      console.log("not parsed");
+    }
+
+    await telegram(JSON.stringify(body, null, 2));
+  }
   return {
     statusCode: 200,
     body: "OK",
